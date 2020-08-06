b6d06e0ef91617fd4a3864a9425840efa87fe06d
alirezaopmc <alirezaopmc@gmail.com>
2020-08-06 23:17:57 +0430

    MergeSort constructor second parameter is no longer initilized.

--- Sorts/MergeSort.js
+++ Sorts/MergeSort.js
@@ -7,7 +7,7 @@ class MergeSort
      * @param {Array} list 
      * @param {function} compare 
      */
-    constructor(list, compare = (a, b) => {return a < b;})
+    constructor(list, compare)
     {
         this._list = sort(list);
         this._compare = compare;
@@ -16,6 +16,66 @@ class MergeSort
     get list()
     {
         return this._list;
+    }
+    
+    /**
+     * 
+     * @param {Array} list
+     * @param {number} left_index 
+     * @param {number} right_index 
+     */
+    sort(list)
+    {
+        if (list.length == 1)
+        {
+            return list;
+        }
+        else
+        {
+            return merge(   
+                sort(list.slice(0, list.length / 2)),
+                sort(list.slice(list.length / 2, this.length))    
+                        );
+        } 
+    }
+
+    /**
+     * 
+     * @param {Array} listA 
+     * @param {Array} listB 
+     */
+    merge(listA, listB)
+    {
+        result = [];
+        indexA = 0;
+        indexB = 0;
+
+        while (indexA < listA.length && indexB < listB.length)
+        {
+            if (this._compare(listA[indexA], listB[indexB]))
+            {
+                result.push(a);
+                indexA++;
+            }
+            else
+            {
+                result.push(b);
+                indexB++;
+            }
+        }
+
+        while (indexA < listA.length)
+        {
+            result.push(a);
+            indexA++;
+        }
+
+        while (indexB < listB.length)
+        {
+            result.push(b);
+            indexB++;
+        }
+        return result;
     }
 
 }
