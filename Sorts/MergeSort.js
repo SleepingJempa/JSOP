1a1e3d34e2bb62e098b7dad4ed9ccab759c78bb9
alirezaopmc <alirezaopmc@gmail.com>
2020-08-06 23:08:44 +0430

    MergeSort class constructor created.

--- Sorts/MergeSort.js
+++ Sorts/MergeSort.js
@@ -2,5 +2,15 @@ class MergeSort
 
 
 {
-    
+    /**
+     * 
+     * @param {Array} list 
+     * @param {function} compare 
+     */
+    constructor(list, compare = (a, b) => {return a < b;})
+    {
+        this._list = sort(list);
+        this._compare = compare;
+    }
+
 }
