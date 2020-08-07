81a812f14224c01a8183555a0a2b43ea1dd22b50
alirezaopmc <alirezaopmc@gmail.com>
2020-08-07 23:00:19 +0430

    LinkedList Node class updated; Property prev added.

--- Data Structure/Stack.js
+++ Data Structure/Stack.js
@@ -41,10 +41,12 @@ class Node
      * 
      * @param {*} value 
      * @param {Node} next 
+     * @param {Node} prev
      */
-    constructor(value, next)
+    constructor(value, prev, next)
     {
         this.value = value;
+        this.prev = prev;
         this.next = next;
     }
 }
