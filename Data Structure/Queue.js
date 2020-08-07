fd21d7fe7e9a50791a3d3b19da05bdd43e539378
alirezaopmc <alirezaopmc@gmail.com>
2020-08-07 23:10:37 +0430

    Queue Node class copied from Stack.

--- Data Structure/Queue.js
+++ Data Structure/Queue.js
@@ -13,5 +13,17 @@ class Node
 
 
 {
-    
+
+    /**
+     * 
+     * @param {*} value 
+     * @param {Node} next 
+     * @param {Node} prev
+     */
+    constructor(value, prev, next)
+    {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
+    }
 }
