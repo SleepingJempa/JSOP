b6603cd7b439cda8e99835ff763aadf64acb07a4
alirezaopmc <alirezaopmc@gmail.com>
2020-08-08 09:12:26 +0430

    BST Node class updated.

--- Data Structure/Trees/BinarySearchTree.js
+++ Data Structure/Trees/BinarySearchTree.js
@@ -11,5 +11,15 @@ class Node
 
 
 {
-    
+    /**
+     * 
+     * @param {*} value 
+     * @param {Node} parent 
+     */
+    constructor(value, parent)
+    {
+        this.value = value;
+        this.parent = parent;
+        this.left = this.rigth = null;
+    }
 }
