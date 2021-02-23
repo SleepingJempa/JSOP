fe2707e2c3f0e54425519329c3bffd737cd58107
Alireza Jafartash <sleepingjempa@outlook.com>
2021-02-23 23:05:07 +0330

    entry file updated

--- run.js
+++ run.js
@@ -1,7 +1,19 @@
 // require('app-module-path').addPath(process.cwd())
 import { addPath } from 'app-module-path'
-addPath(__dirname)
+addPath(process.cwd())
 
-import * as DS from './data-structures'
+import * as DS from './data-structures/index.js'
 
 let g = new DS.Graph()
+
+let n = 10
+for(let i = 0; i < n; i++) {
+    g.add()
+}
+
+g.connect(0, 5)
+g.connect(5, 3)
+g.connect(3, 7)
+g.connect(6, 9)
+
+g.
