e066d2a1efe9479432f766183a6bba00cd436cf8
Alireza Jafartash <sleepingjempa@outlook.com>
2021-02-23 17:49:28 +0330

    Node class created

---
+++ data-structures/node/index.js
@@ -0,0 +1,53 @@
+class Node {
+    /**
+     * Constructor for Node class
+     * @param {Object} data what will be saved on this node
+     * @param {Number} index the index of this node
+     */
+    constructor(data = {}, index) {
+        this._data = data
+        this._index = index
+        this._adj = []
+    }
+
+    /**
+     * Add the other node to the adjacency list
+     * @param {Node} other
+     */
+    connect(other) {
+        this._adj.push(other)
+    }
+
+    /**
+     * Return the list of adjacent nodes
+     */
+    adjacentList() {
+        return this._adj
+    }
+
+    /**
+     * Return the index of this node in the vertices list
+     */
+    index() {
+        return this._index
+    }
+
+    /**
+     * Getter the data object
+     * @param {String} property name of the property
+     */
+    get(property) {
+        return this._data[property]
+    }
+    
+    /**
+     * Set the (new) value to the property (if exists)
+     * @param {String} property 
+     * @param {*} value
+     */
+    set(property, value) {
+        this._data[property] = value
+    }
+}
+
+module.exports = Node
